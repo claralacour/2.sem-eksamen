289ae9668d0ed6ef3309e0519aabdcf4fb6bc366
Stikkelmaal <119247878+claralacour@users.noreply.github.com>
2023-05-24 13:05:02 +0200

    sammenligning igang

---
+++ compare.js
@@ -0,0 +1,8 @@
+let compareBox = document.getElementById("compare");
+
+//den her funktion sørger for at sammenligningsboxen bliver vist, når der bliver trykket på knappen, med
+//vores lille animation wuhu 
+function show(){
+    compareBox.classList.toggle("hide");
+}
+

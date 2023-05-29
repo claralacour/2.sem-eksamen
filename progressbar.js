4ce1a1dd536d883c63067619dda2ae252e4dcfef
claralacour <denlacour@gmail.com>
2023-05-29 10:37:12 +0200

    haha

--- progressbar.js
+++ progressbar.js
@@ -2,7 +2,7 @@ function updateProgressBar(){
     const{scrollTop, scrollHeight} = document.documentElement;
     const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
     console.log(scrollPercent);
-    //update progress var to the total height
+    //update progress bar to the total height
     document.getElementById("progress-bar").style.setProperty('--progress', scrollPercent);
 
 }

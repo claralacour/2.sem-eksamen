bc85743a32ce943e73dd498cf89f0d4cefc7180d
claralacour <denlacour@gmail.com>
2023-06-05 12:53:22 +0200

    list showing up yay

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,5 @@
 console.log("js is running...");
 
-let benzinInput = document.getElementById("dinBenzin");
 const benzinList = ["VW T-Roc", "Citroën C4 Cactus", "Ford Fiesta", "Kia Rio", "Audi A1", "Fiat Punto"];
 
 
@@ -12,8 +11,35 @@ function autoComplete(benzinInput, benzinList){
     benzinInput.addEventListener('input', function(){
         //luk listen hvis den allerede er åben
         closeList();
+
         //hvis input er tomt, så luk funktionen(og "this" er noget man bruger fordi det er i konteksten af det der allerede sker, altså inputtet)
         if (!this.value)
             return;
-    })
+
+        //her laver vi en suggestions div
+        suggestions = document.createElement('div');
+        //her giver vi div'en id'et "suggestions"
+        suggestions.setAttribute('id', 'suggestions');
+        //og her siger vi at det er et child element til "this"(altså vores benzinInput)
+        this.parentNode.appendChild(suggestions);
+
+        //så looper vi gennem vores array og finder matches 
+        for (let i=0; i<benzinList.length; i++){
+            //hvis benzinList inkludere værdien i inputfeltet skydes den næste kodeblok igang
+            if (benzinList[i].toUpperCase().includes(this.value.toUpperCase())){
+                //først skabes en div der viser én suggestion
+                suggestion = document.createElement('div');
+                //dernæst ændrer vi teksten i listen til at være det der står i vores array
+                suggestion.innerHTML = benzinList[i];
+                //til sidst siger vi at det er et child element til "suggestions", det gør den til en del af vores første liste
+                suggestions.appendChild(suggestion);
+            }
+        }
+    });
+    function closeList(){
+        let suggestions = document.getElementById("suggestions");
+        if (suggestions)
+            suggestions.parentNode.removeChild(suggestions);
+    }
 }
+autoComplete(document.getElementById('dinbenzin'), benzinList);
